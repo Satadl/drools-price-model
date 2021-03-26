812e72331ecb52f48bdcefd9fe6f4b54a8999217
admin <>
2021-03-26 17:06:46 +0000

    {/src/main/java/com/wakandaspace/drools_price_model/plan.java}

--- src/main/java/com/wakandaspace/drools_price_model/plan.java
+++ src/main/java/com/wakandaspace/drools_price_model/plan.java
@@ -6,12 +6,47 @@ package com.wakandaspace.drools_price_model;
 
 public class plan implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public plan() {
-    }
+	@org.kie.api.definition.type.Label(value = "name")
+	private java.lang.String name;
+	@org.kie.api.definition.type.Label(value = "hospitalTier")
+	private java.lang.String hospitalTier;
+	@org.kie.api.definition.type.Label(value = "benefitLimits")
+	private double benefitLimits;
 
+	public plan() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.String getHospitalTier() {
+		return this.hospitalTier;
+	}
+
+	public void setHospitalTier(java.lang.String hospitalTier) {
+		this.hospitalTier = hospitalTier;
+	}
+
+	public double getBenefitLimits() {
+		return this.benefitLimits;
+	}
+
+	public void setBenefitLimits(double benefitLimits) {
+		this.benefitLimits = benefitLimits;
+	}
+
+	public plan(java.lang.String name, java.lang.String hospitalTier,
+			double benefitLimits) {
+		this.name = name;
+		this.hospitalTier = hospitalTier;
+		this.benefitLimits = benefitLimits;
+	}
 
 }
