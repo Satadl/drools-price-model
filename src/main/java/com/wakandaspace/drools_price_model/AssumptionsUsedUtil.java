d3ef5abb6c1fd12170597043f83da00328b51db4
admin <>
2021-03-30 11:21:19 +0000

    {/src/main/java/com/wakandaspace/drools_price_model/AssumptionsUsedUtil.java}

--- src/main/java/com/wakandaspace/drools_price_model/AssumptionsUsedUtil.java
+++ src/main/java/com/wakandaspace/drools_price_model/AssumptionsUsedUtil.java
@@ -6,12 +6,14 @@ package com.wakandaspace.drools_price_model;
 
 public class AssumptionsUsedUtil implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public AssumptionsUsedUtil() {
-    }
+	public AssumptionsUsedUtil() {
+	}
 
-
-
-
+	public static java.util.List<com.wakandaspace.drools_price_model.AssumptionsUsed> getAssumptionsUsedList = new java.util.ArrayList<com.wakandaspace.drools_price_model.AssumptionsUsed>() {
+		{
+			add(new AssumptionsUsed("Renal", "1 Session", 0, 79));
+		}
+	};
 }
