a6af99c5cba98d764f195c56c5dd6e74a3bef2c8
admin <>
2021-04-07 15:49:56 +0000

    {/src/main/java/com/wakandaspace/drools_price_model/BenefitUtil.java}

--- src/main/java/com/wakandaspace/drools_price_model/BenefitUtil.java
+++ src/main/java/com/wakandaspace/drools_price_model/BenefitUtil.java
@@ -6,12 +6,18 @@ package com.wakandaspace.drools_price_model;
 
 public class BenefitUtil implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public BenefitUtil() {
-    }
+	public BenefitUtil() {
+	}
+	public static java.util.List<com.wakandaspace.drools_price_model.Benefit> getRedBerylBenefits = new java.util.ArrayList<com.wakandaspace.drools_price_model.Benefit>() {
+		{
+			add(new Benefit("Basic outpatient and in-patient Treatment",
+					"Covered", "Red Berryl Lite"));
+			add(new Benefit("Ostetrician", "Covered", "Red Berryl Lite"));
+			add(new Benefit("Dialysis and all related care", "1 session",
+					"Red Berryl Lite"));
 
-
-
-
+		}
+	};
 }
