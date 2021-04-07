3464dfc85793331c729a22e6f33b8fc26c7c4cf7
admin <>
2021-04-07 14:57:10 +0000

    {/src/main/java/com/wakandaspace/drools_price_model/BenefitUtil.java}

--- src/main/java/com/wakandaspace/drools_price_model/BenefitUtil.java
+++ src/main/java/com/wakandaspace/drools_price_model/BenefitUtil.java
@@ -11,15 +11,4 @@ public class BenefitUtil implements java.io.Serializable {
 	public BenefitUtil() {
 	}
 
-	public static java.util.List<com.wakandaspace.drools_price_model.Benefit> getRedBerylBenefits = new java.util.ArrayList<com.wakandaspace.drools_price_model.Benefit>() {
-		{
-			add(new Benefit("Basic outpatient and in-patient Treatment",
-					"covered", "", "", 0));
-			add(new Benefit("Ostetrician", "covered", "unlimited", "", 0));
-			add(new Benefit("Dialysis and all related care", "covered",
-					"1 session", "", 0));
-
-		}
-	};
-
 }
