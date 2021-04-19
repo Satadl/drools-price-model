16aab8482875de1f2c8c693fcd5bf1767739f563
admin <>
2021-04-19 14:38:54 +0000

    {/src/main/java/com/wakandaspace/drools_price_model/Plan.java}

--- src/main/java/com/wakandaspace/drools_price_model/Plan.java
+++ src/main/java/com/wakandaspace/drools_price_model/Plan.java
@@ -13,7 +13,7 @@ public class Plan implements java.io.Serializable {
 	@org.kie.api.definition.type.Label("hospitalTier")
 	private java.lang.String hospitalTier;
 	@org.kie.api.definition.type.Label("individualPremiumResult")
-	private double individualPremiumResult = 2.0;
+	private double individualPremiumResult;
 	@org.kie.api.definition.type.Label("name")
 	private java.lang.String name;
 	@org.kie.api.definition.type.Label("preAuthorizationRequired")
