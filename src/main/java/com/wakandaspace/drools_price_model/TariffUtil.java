c1d8f1451c179a49253673a24881d83bd99bf100
admin <>
2021-04-22 10:59:49 +0000

    {/src/main/java/com/wakandaspace/drools_price_model/TariffUtil.java}

--- src/main/java/com/wakandaspace/drools_price_model/TariffUtil.java
+++ src/main/java/com/wakandaspace/drools_price_model/TariffUtil.java
@@ -15,6 +15,7 @@ public class TariffUtil implements java.io.Serializable {
 		{
 			add(new Tariff("Basic outpatient and in-patient Treatment", 2000.0,
 					1500.0, 400.0, 400.0, 1250.0));
+			// Specialist consultation
 			add(new Tariff("Obstetrician", 150000.0, 100000.0, 26700.0,
 					16700.0, 25.0));
 			add(new Tariff("Gynaecologist", 150000.0, 1000000.0, 26700.0,
@@ -42,6 +43,71 @@ public class TariffUtil implements java.io.Serializable {
 					33400.0, 6.0));
 			add(new Tariff("Psychiatrist", 250000.0, 225000.0, 66700.0,
 					33400.0, 4.0));
+			add(new Tariff("Neonatologist", 60000.0, 50000.0, 11700.0, 11700.0,
+					3.0));
+			add(new Tariff("Dermatologist", 250000.0, 225000.0, 66700.0,
+					33400.0, 10.0));
+			add(new Tariff("Haematologist", 45000.0, 360000, 9400.0, 8400.0,
+					3.0));
+			add(new Tariff("Dietician", 250000.0, 225000.0, 66700.0, 33400.0,
+					4.0));
+			add(new Tariff("Nutritionist", 250000.0, 225000.0, 66700.0,
+					33400.0, 4.0));
+			add(new Tariff("Pulmonologist", 250000.0, 225000.0, 66700.0,
+					33400.0, 3.0));
+			add(new Tariff("Respiratory Physician", 250000.0, 225000.0,
+					66700.0, 33400.0, 3.0));
+			add(new Tariff("Hematologist", 250000.0, 225000.0, 66700.0,
+					33400.0, 3.0));
+			add(new Tariff("Oncologist", 100000.0, 75000.0, 16700.0, 16700.0,
+					15.0));
+			add(new Tariff("Pathologist", 250000.0, 225000.0, 66700.0, 33400.0,
+					23.0));
+			add(new Tariff("Endocrinologist", 250000.0, 225000.0, 66700.0,
+					33400.0, 134.0));
+			add(new Tariff("Family Physician", 250000.0, 225000.0, 66700.0,
+					33400.0, 45.0));
+			add(new Tariff("Oral and Maxilofacial Surgeon", 250000.0, 225000.0,
+					66700.0, 33400.0, 3.0));
+			// Access to free telemedecine app
+			add(new Tariff("Chat-certified Doctors during medical emergency",
+					500.0, 500.0, 500.0, 500.0, 1000.0));
+			add(new Tariff("Chat-certified Doctors for medical Information",
+					500.0, 500.0, 500.0, 500.0, 1000.0));
+			add(new Tariff("Free drug Pick up at designated Pharmacies", 500.0,
+					500.0, 500.0, 500.0, 1000.0));
+			add(new Tariff("GPS-enabled access to hospital directories", 500.0,
+					500.0, 500.0, 500.0, 1000.0));
+			add(new Tariff("Telemedecine app with details of covered benefits",
+					500.0, 500.0, 500.0, 500.0, 1000.0));
+			// Accident and emergency care
+			add(new Tariff("Resuscitative care needed to stabilize patient",
+					12000.0, 10000.0, 3000.0, 2500.0, 50.0));
+			// Basic Diagnostic imaging
+			add(new Tariff("Chest X-Rays", 9500.0, 9000.0, 2700.0, 2500.0, 23.0));
+			add(new Tariff("Abdominal X-Rays", 12000.0, 10000.0, 3200.0,
+					2700.0, 85.0));
+			add(new Tariff("Limbs X-Rays", 4000.0, 2500.0, 700.0, 700.0, 15.0));
+			add(new Tariff("Neck X-Rays", 4000.0, 2500.0, 700.0, 700.0, 23.0));
+			add(new Tariff("Sinus X-Rays", 4000.0, 2500.0, 700.0, 700.0, 12.0));
+			add(new Tariff("Mastoid X-Rays", 4000.0, 2500.0, 700.0, 700.0, 15.0));
+			add(new Tariff("Cervical Spine X-Rays", 4000.0, 2500.0, 700.0,
+					700.0, 19.0));
+			add(new Tariff("Skull X-Rays", 4000.0, 2500.0, 700.0, 700.0, 2.5));
+			add(new Tariff("Pelvic X-Rays", 4000.0, 2500.0, 700.0, 700.0, 11.0));
+			add(new Tariff("Thoracic Inlet X-Rays", 4000.0, 2500.0, 700.0,
+					700.0, 3.0));
+			add(new Tariff("Thoraco-Lumbar X-Rays", 4000.0, 2500.0, 700.0,
+					700.0, 2.0));
+			add(new Tariff("Lumbosacral X-Rays", 4000.0, 2500.0, 700.0, 700.0,
+					2.0));
+			add(new Tariff("Mandibles/Temporomandibular Joint X-Rays", 4000.0,
+					2500.0, 700.0, 700.0, 33.0));
+			add(new Tariff("X-Rays of All Body Joints", 4000.0, 2500.0, 700.0,
+					700.0, 2.4));
+			add(new Tariff("Ultrasound Scan", 4000.0, 2500.0, 700.0, 700.0,
+					32.0));
+
 			add(new Tariff("Dialysis and all related care", 225000.0, 220000.0,
 					205000.0, 200000.0, 1.5));
 		}
